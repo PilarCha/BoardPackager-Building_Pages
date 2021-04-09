3a7a84060f01b95d28d575e8b7f955a892b30601
PilarCha <pchavez92@gmail.com>
2021-04-08 22:15:34 -0700

    made the root page go to Building index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'building/index'
+  root 'building#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
