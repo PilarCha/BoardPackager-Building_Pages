74fd403e396abe2f6200389849536d5e2f3bbb57
PilarCha <pchavez92@gmail.com>
2021-04-17 12:55:14 -0700

    post interview. Updated controller to more ruby way

--- app/controllers/building_controller.rb
+++ app/controllers/building_controller.rb
@@ -1,10 +1,13 @@
 class BuildingController < ApplicationController
   def index
     @Buildings = Building.all
+    # instances/var lowercase
   end
 
   def show
     @Building = Building.find(params[:id])
-    @Apartments = Apartment.where("building_id = #{params[:id]}")
+    @Apartments = @Building.Apartment.where("building_id = #{params[:id].to_i}")
+    # Apartment.where(building_id: params[:id]) throws error
+    # Apartment.where(building_id: params[:id].to_i) better
   end
 end
